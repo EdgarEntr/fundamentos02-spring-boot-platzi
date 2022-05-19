4987c638f9cec80de288db95a6220186ab5c321a
EdgarEntr <edgar9428@hotmail.com>
2022-05-19 13:18:15 -0500

    first commit

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos02/Fundamentos02Application.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamentos02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fundamentos02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fundamentos02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/springboot/fundamentos02/Fundamentos02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamentos02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fundamentos02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
